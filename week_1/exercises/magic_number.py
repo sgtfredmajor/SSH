6ec3d0ee03b4ebab242385b8c325bbb6ba5304d8
Fred Major <sgtfredmajor@gmail.com>
2020-01-15 14:57:28 -0500

    Added week 1 class exercises

---
+++ week_1/exercises/magic_number.py
@@ -0,0 +1,14 @@
+# Prompt the user for a number
+# compair that number to predefined value
+# If the nembers match, tell the user there a mind reader
+# If they dont match, thell the user "thanks for playing"
+
+user_input = int(input("what number am I thinking of? "))
+
+MAGIGNUMBER = 0
+
+if user_input == MAGIGNUMBER:
+    print("ARE YOU A MIND READER??? MINDFREAK!!")
+
+else:
+    print("Thanks for playing, thats not correct")
---
+++ week_1/exercises/prompt_for_number.py
@@ -0,0 +1,11 @@
+# Run a program that prompts the user for a number and then multiplies
+# that number by itself
+
+# This is the first step, prompting a user
+user_input = int(input("Please enter a number: "))
+
+#This is the second step, multilying the imput
+result = user_input * user_input 
+
+# Final step, return the result
+print(result)
